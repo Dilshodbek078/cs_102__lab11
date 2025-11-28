3abde3be9131e85823eb30d765dd42e93871ddb8
Dilshodbek078 <d.orazbayev@newuu.uz>
2025-11-28 23:28:45 +0500

    Create problem5.cpp

---
+++ problem5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void fixNeg(int* arr, int size) {
+    int* p = arr;
+    int* end = arr + size;
+    while (p < end) {
+        if (*p < 0) *p = 0;
+        p++;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    fixNeg(arr, n);
+
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+}
